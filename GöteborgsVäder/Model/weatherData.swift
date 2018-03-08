834da1f1c1db90b15af5c9a93c07eee2c053bf09
jsonkuan <jsonkuan@gmail.com>
2018-03-08 09:48:34 +0100

    Setup project

---
+++ GöteborgsVäder/Model/weatherData.swift
@@ -0,0 +1,15 @@
+//
+//  weatherData.swift
+//  GöteborgsVäder
+//
+//  Created by Jason Kuan on 2018-03-08.
+//  Copyright © 2018 Jason Kuan. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData {
+	
+	var summary: String
+	var icon: String
+}
---
+++ GöteborgsVäder/ViewController/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  GöteborgsVäder
+//
+//  Created by Jason Kuan on 2018-03-08.
+//  Copyright © 2018 Jason Kuan. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+	@IBOutlet weak var weatherIcon: UIImageView!
+	@IBOutlet weak var weatherLabel: UILabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+
+}
+
